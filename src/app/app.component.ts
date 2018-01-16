55f16b818c6cda277dc96ed5333cd0ce61af4a31
Alkari <colbobtfish@gmail.com>
2018-01-15 21:25:41 -0600

    Chap1 intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+//aplication wide services
+//@ decorator symbol actually compiles to a function call in javascript
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular App Hello World Test';
 }
